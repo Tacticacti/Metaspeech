fc3dcffe057efbbaccaf5d985ac4dc5e53edb289
Mateo <M.A.Nasse@student.tudelft.nl>
2024-05-03 17:24:26 +0200

    Added modules for testing to dev dependencies and added tests to vitest-example for reference

--- graphgadget/src/routes/vitest-example/+page.svelte
+++ graphgadget/src/routes/vitest-example/+page.svelte
@@ -1,12 +1,22 @@
 <script lang="ts">
     export let currency: number = 0
+    let counter = 0
+    $: doubled = counter * 2
 
     function formatCurrency(currency: number) {
         return `$${currency}`
     }
+
+    function incrememt() {
+        counter += 1
+    }
+
 </script>
 
 <h1>Only meant to be used as example for testing purposes</h1>
 <h3>Either run "npm test" or "npm run test:unit" to unit test</h3>
 <input type="text" placeholder="input number" bind:value={currency}>
 <div data-testid="formatted-currency">{formatCurrency(currency)}</div>
+<button on:click={incrememt}>Increase counter</button>
+<div data-testid="counter">Counter is: {counter}</div>
+<div data-testid="doubled">Double is: {doubled}</div>
--- graphgadget/src/routes/vitest-example/page.test.ts
+++ graphgadget/src/routes/vitest-example/page.test.ts
@@ -1,8 +1,7 @@
 import { it, expect } from "vitest"
-import { render } from '@testing-library/svelte';
+import { render } from '@testing-library/svelte'
+import userEvent from '@testing-library/user-event'
 import sut from "./+page.svelte"
-
-// @ts-nocheck
 
 it("displays the currency with a $ in front of it", () => {
     const {getByTestId} = render(sut, {currency: 20})
@@ -10,4 +9,68 @@ it("displays the currency with a $ in front of it", () => {
     expect(
         getByTestId("formatted-currency").innerHTML
     ).toEqual("$20")
+})
+
+it("displays the default value of currency", () => {
+    const {getByTestId, getByRole} = render(sut)
+
+    expect(getByTestId("formatted-currency").innerHTML).toEqual("$0")
+
+    expect(getByRole("textbox").innerHTML).toEqual("")
+})
+
+it("displays the default value of currency after input has changed", async () => {
+    const user = userEvent.setup()
+
+    const {getByRole, getByTestId} = render(sut)
+
+    const formatted = getByTestId("formatted-currency")
+    const input = getByRole("textbox")
+    user.clear(input)
+    await user.type(input, "50")
+
+    expect(formatted.innerHTML).toEqual("$50")
+})
+
+it("displays the main heading", () => {
+    const {getByRole} = render(sut, {currency: 10})
+
+    const text = "Only meant to be used as example for testing purposes"
+    const heading = getByRole("heading", {name: text}) 
+
+    expect(heading).to.exist
+})
+
+it("displays the h3 heading", () => {
+    const {getByRole} = render(sut, {currency: 2})
+
+    const text = 'Either run "npm test" or "npm run test:unit" to unit test'
+    const heading = getByRole("heading", {name: text, level:3}) 
+
+    expect(heading).to.exist
+})
+
+it("counter starts at 0", () => {
+    const {getByTestId} = render(sut)
+
+    const counter = getByTestId("counter")
+    const doubled = getByTestId("doubled")
+
+    expect(counter.innerHTML).toContain("0")
+    expect(doubled.innerHTML).toContain("0")
+})
+
+it("counter starts at 0", async () => {
+    const user = userEvent.setup()
+
+    const {getByTestId, getByRole} = render(sut)
+
+    const counter = getByTestId("counter")
+    const doubled = getByTestId("doubled")
+    const button = getByRole("button")
+
+    await user.click(button)
+
+    expect(counter.innerHTML).toContain("1")
+    expect(doubled.innerHTML).toContain("2")
 })
