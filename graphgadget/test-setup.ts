169f46c7e64bb1dad943c33e1a642064277f1a3a
Jang Hun Oh <edwardoh@tudelft.nl>
2024-05-09 14:08:21 +0200

    Added test for TableView.svelte using Vitest

---
+++ graphgadget/test-setup.ts
@@ -0,0 +1,7 @@
+// test-setup.ts
+import { expect } from 'vitest';
+import * as customMatchers from '@testing-library/jest-dom/matchers';
+
+Object.entries(customMatchers).forEach(([name, matcher]) => {
+  expect.extend({ [name]: matcher });
+});
