c546b8dce74d5bad96e60ede40a6a8d29e864306
Boris Annink <boris.annink@gmail.com>
2024-05-09 13:25:37 +0200

    AAAAAAAAAAAAAAAA

--- graphgadget/tests/test.ts
+++ graphgadget/tests/test.ts
@@ -2,12 +2,7 @@ import { expect, test } from '@playwright/test';
 
 test('should render', async ({ page }) => {
 	await page.goto('/');
-<<<<<<< HEAD
 	//get file input
 	const input = await page.$('input[type=file]');
 	expect(input).not.toBeNull();
-=======
-	await expect(page.getByRole('input'));
-	await expect(page.getByRole('heading', { name: 'Welcome to SvelteKit' })).toBeVisible();
->>>>>>> 133c8a67424cb0676e753f26683c61c51fdd39d7
 });
