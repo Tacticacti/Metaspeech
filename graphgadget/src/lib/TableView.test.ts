ce0333831330dc35868e5b7ab6bda4a78e0503ed
Jang Hun Oh <edwardoh@tudelft.nl>
2024-05-09 14:31:41 +0200

    Fixed checkstyle

--- graphgadget/src/lib/TableView.test.ts
+++ graphgadget/src/lib/TableView.test.ts
@@ -4,24 +4,27 @@ import DataFrame from 'dataframe-js';
 import '@testing-library/jest-dom';
 
 describe('TableView', () => {
-  it('should render a table with headers and rows based on provided data', async () => {
-    // Create a mock DataFrame
-    const mockData = new DataFrame([
-      { col1: 'Row1Col1', col2: 'Row1Col2' },
-      { col1: 'Row2Col1', col2: 'Row2Col2' }
-    ], ['col1', 'col2']);
+	it('should render a table with headers and rows based on provided data', async () => {
+		// Create a mock DataFrame
+		const mockData = new DataFrame(
+			[
+				{ col1: 'Row1Col1', col2: 'Row1Col2' },
+				{ col1: 'Row2Col1', col2: 'Row2Col2' }
+			],
+			['col1', 'col2']
+		);
 
-    // Render the component with mock data
-    const { getByText } = render(TableView, { data: mockData });
+		// Render the component with mock data
+		const { getByText } = render(TableView, { data: mockData });
 
-    // Check if headers are present
-    expect(getByText('col1')).toBeInTheDocument();
-    expect(getByText('col2')).toBeInTheDocument();
+		// Check if headers are present
+		expect(getByText('col1')).toBeInTheDocument();
+		expect(getByText('col2')).toBeInTheDocument();
 
-    // Check if data is present
-    expect(getByText('Row1Col1')).toBeInTheDocument();
-    expect(getByText('Row1Col2')).toBeInTheDocument();
-    expect(getByText('Row2Col1')).toBeInTheDocument();
-    expect(getByText('Row2Col2')).toBeInTheDocument();
-  });
+		// Check if data is present
+		expect(getByText('Row1Col1')).toBeInTheDocument();
+		expect(getByText('Row1Col2')).toBeInTheDocument();
+		expect(getByText('Row2Col1')).toBeInTheDocument();
+		expect(getByText('Row2Col2')).toBeInTheDocument();
+	});
 });
--- graphgadget/test-setup.ts
+++ graphgadget/test-setup.ts
@@ -3,5 +3,5 @@ import { expect } from 'vitest';
 import * as customMatchers from '@testing-library/jest-dom/matchers';
 
 Object.entries(customMatchers).forEach(([name, matcher]) => {
-  expect.extend({ [name]: matcher });
+	expect.extend({ [name]: matcher });
 });
