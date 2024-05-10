17248f1d6ee2446ef87413fcf0e3ce2885437337
Jang Hun Oh <edwardoh@tudelft.nl>
2024-05-10 16:59:15 +0200

    Achieved 100% test coverage for FileParser.ts file.

--- graphgadget/src/lib/importer/scripts/FileParser.test.ts
+++ graphgadget/src/lib/importer/scripts/FileParser.test.ts
@@ -1,6 +1,6 @@
-import { it, expect } from 'vitest';
-import { Parse } from './FileParser';
+import { it, expect, vi } from 'vitest';
 import DataFrame from 'dataframe-js';
+import { Parse, ParseXls, GetFileExtension } from './FileParser';
 
 // Function to create and write data to a TSV file
 function createTSVFile(data: string[][]) {
@@ -8,6 +8,63 @@ function createTSVFile(data: string[][]) {
 	const tsvData = data.map((row) => row.join('\t')).join('\n');
 	return [tsvData];
 }
+
+it('should return the correct file extension for standard file names', () => {
+	const file = new File([], 'example.txt');
+	expect(GetFileExtension(file)).toBe('txt');
+});
+
+it('should return the correct file extension for file names with multiple dots', () => {
+	const file = new File([], 'example.with.multiple.dots.jpg');
+	expect(GetFileExtension(file)).toBe('jpg');
+});
+
+it('should return an empty string for files without an extension', () => {
+	const file = new File([], 'example');
+	expect(GetFileExtension(file)).toBe('');
+});
+
+it('should return an empty string for files with a dot but no extension', () => {
+	const file = new File([], 'example.');
+	expect(GetFileExtension(file)).toBe('');
+});
+
+it('should return the correct file extension for files with complex names', () => {
+	const file = new File([], 'complex_example_file.tar.gz');
+	expect(GetFileExtension(file)).toBe('gz');
+});
+
+it('should handle cases with null or undefined file names', () => {
+	const file = new File([], ''); // Simulate an empty file name scenario
+	expect(GetFileExtension(file)).toBe('');
+});
+
+it('should return the correct file extension regardless of case sensitivity', () => {
+	const file = new File([], 'EXAMPLE.PDF');
+	expect(GetFileExtension(file)).toBe('PDF');
+});
+
+vi.mock('./XlsParser', () => ({
+	ParseXls: vi.fn(() => Promise.resolve(new DataFrame([])))
+}));
+
+it('should correctly parse .xls files', async () => {
+	const file = new File(['dummy content'], 'test.xls', { type: 'application/vnd.ms-excel' });
+	const result = Parse(file);
+	expect(result).toBeInstanceOf(Promise);
+	await expect(result).resolves.toBeInstanceOf(DataFrame);
+	expect(ParseXls).toHaveBeenCalled();
+});
+
+it('should correctly parse .xlsx files', async () => {
+	const file = new File(['dummy content'], 'test.xlsx', {
+		type: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
+	});
+	const result = Parse(file);
+	expect(result).toBeInstanceOf(Promise);
+	await expect(result).resolves.toBeInstanceOf(DataFrame);
+	expect(ParseXls).toHaveBeenCalled();
+});
 
 it('importer pass tsv', async () => {
 	const testData = [
