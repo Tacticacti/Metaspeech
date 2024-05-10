e81b262139c5909a049e3ce6d03e6eff9a083f9a
Pedro Gomes Moreira <p.gomesmoreira@student.tudelft.nl>
2024-05-10 11:45:06 +0200

    used svelte:head

--- graphgadget/src/lib/pages/Start.svelte
+++ graphgadget/src/lib/pages/Start.svelte
@@ -9,33 +9,31 @@
 	}
 </script>
 
-<head>
+<svelte:head>
 	<title>Graph Gadget</title>
-</head>
+</svelte:head>
 
-<body>
-	<header>
-		<h1>Graph Gadget</h1>
-	</header>
-	<nav>
-		<ul>
-			<li>
-				<Importer on:input={handleInput} />
-			</li>
+<header>
+	<h1>Graph Gadget</h1>
+</header>
+<nav>
+	<ul>
+		<li>
+			<Importer on:input={handleInput} />
+		</li>
 
-			<li><a href=".">Previous Data</a></li>
-		</ul>
-	</nav>
-	<main>
-		<h3>How it works</h3>
-		<h2>An intuitive way to visualize your speech metadata</h2>
-		<div>
-			<div>Upload your data as a TSV, JSON, XLS, or TXT in the correct format</div>
-			<div>Select which parameters and graph you want us to create</div>
-			<div>See the resulting graph on your browser or download it as a JPEG or PNG</div>
-		</div>
-		<footer>
-			<p>Copyright Graph Gadget</p>
-		</footer>
-	</main>
-</body>
+		<li><a href=".">Previous Data</a></li>
+	</ul>
+</nav>
+<main>
+	<h3>How it works</h3>
+	<h2>An intuitive way to visualize your speech metadata</h2>
+	<div>
+		<div>Upload your data as a TSV, JSON, XLS, or TXT in the correct format</div>
+		<div>Select which parameters and graph you want us to create</div>
+		<div>See the resulting graph on your browser or download it as a JPEG or PNG</div>
+	</div>
+	<footer>
+		<p>Copyright Graph Gadget</p>
+	</footer>
+</main>
