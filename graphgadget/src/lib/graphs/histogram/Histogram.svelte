1106a0af057e840cdc487134748d69f23e5c46ca
Pedro Gomes Moreira <p.gomesmoreira@student.tudelft.nl>
2024-05-16 18:17:51 +0200

    Changes from MR

--- graphgadget/src/lib/graphs/histogram/Histogram.svelte
+++ graphgadget/src/lib/graphs/histogram/Histogram.svelte
@@ -6,138 +6,25 @@
 	import { setColor } from '$lib/utils/CanvasUtils';
 	import { afterUpdate, onMount } from 'svelte';
 	import ParameterSelector from '$lib/graphs/histogram/ParameterSelector.svelte';
+	import {
+		getNumericalColumns,
+		calculateAxis,
+		sortParallelArrays
+	} from '$lib/graphs/histogram/HistogramController';
 
 	let canvas: HTMLCanvasElement;
 	let chart: Chart;
 
 	const columnNames = $data.listColumns() as string[];
-	const numericColumnNames = getNumericalColumns(columnNames);
 
-	let selectedParams: string[] = [];
-	let checkedMean = false;
-	let parameterType = 'Absolute Frequency';
+	// TODO: find a better way to get the type,
+	// currently only looks at first row.
+	// Remember there can be undefined elements if not cleared
+	const numericColumnNames = getNumericalColumns(columnNames, $data.getRow(0));
 
-	export function getNumericalColumns(columnNames: string[]): string[] {
-		// TODO: find a better way to get the type,
-		// currently only looks at first element.
-		// Remember there can be undefined elements if not cleared
-		const columnTypes = new Map<string, string>(
-			columnNames.map((name) => [name, typeof $data.toArray(name)[0]])
-		);
-		const numericColumnNames = columnNames.filter((name) => columnTypes.get(name) === 'number');
-
-		return numericColumnNames;
-	}
-
-	export function calculateAxis(
-		/* eslint-disable @typescript-eslint/no-explicit-any */
-		dataRows: any[],
-		selectedParams: string[],
-		checkedMean: boolean,
-		yAxisParam: string
-	): [string[], number[]] {
-		let mapFrequencies = new Map<string, number>();
-		let mapValues = new Map<string, number>();
-
-		// Iterate all rows
-		for (const row of dataRows) {
-			// Aggregate all selected columns of that row in a comma-separated string
-			// Used as key for the maps, basically represents a bar in the chart
-			const xValues: string = selectedParams
-				.map(paramName => row[paramName] ?? '<empty>')
-				.join(', ');
-
-			// Get the current frequency in the map with that string as key
-			const currentFrequency: number | undefined = mapFrequencies.get(xValues);
-			// Increment the frequency of that key
-			mapFrequencies.set(xValues, currentFrequency === undefined ? 1 : currentFrequency + 1);
-
-			if (yAxisParam != 'Absolute Frequency' && yAxisParam != 'Relative Frequency') {
-				// Need to increment value of the selected y-axis parameter
-				const valueIncrement = row[yAxisParam];
-
-				if (valueIncrement === undefined) {
-					// Skip this value entirely from the calculation
-					// So decrement its frequency (which cannot be null, or undefined)
-					mapFrequencies.set(xValues, mapFrequencies.get(xValues)! - 1);
-				} else {
-					const currentValue = mapValues.get(xValues);
-					mapValues.set(
-						xValues,
-						currentValue === undefined ? valueIncrement : currentValue + valueIncrement
-					);
-				}
-			}
-		}
-
-		// convert map to arrays
-		const labels: string[] = [...mapFrequencies.keys()];
-		let frequencies: number[] = [...mapFrequencies.values()];
-
-		if (yAxisParam == 'Absolute Frequency') {
-			return [labels, frequencies];
-		}
-		if (yAxisParam == 'Relative Frequency') {
-			const totalFrequency = dataRows.length;
-			return [labels, frequencies.map((f) => f / totalFrequency)];
-		}
-
-		const values: number[] = [...mapValues.values()];
-
-		if (checkedMean) {
-			// Calculate means of values
-			for (let idx = 0; idx < values.length; ++idx) {
-				values[idx] /= frequencies[idx];
-			}
-		}
-
-		return [labels, values];
-	}
-
-	// Sorts two arrays based on the labels
-	export function sortParallelArrays(labels: string[], values: number[]): [string[], number[]] {
-		let combinedArray: [string, number][] = [];
-		for (let idx = 0; idx < labels.length; ++idx) {
-			combinedArray.push([labels[idx], values[idx]]);
-		}
-
-		combinedArray.sort((a, b) => {
-			const paramsA = a[0].split(', ');
-			const paramsB = b[0].split(', ');
-
-			for (let idx = 0; idx < paramsA.length; ++idx) {
-				let comparison = 0;
-
-				const [la, lb] = [paramsA[idx], paramsB[idx]];
-
-				if (!isNaN(+la) && !isNaN(+lb)) {
-					// Numeric field
-					comparison = +la - +lb;
-				} else {
-					// String field
-					comparison = la.localeCompare(lb);
-				}
-
-				if (comparison != 0) {
-					// If not equal, return number
-					return comparison;
-				}
-			}
-
-			// If all equal, they are equal
-			return 0;
-		});
-
-		let newLabels: string[] = [];
-		let newValues: number[] = [];
-
-		for (const [lbl, val] of combinedArray) {
-			newLabels.push(lbl);
-			newValues.push(val);
-		}
-
-		return [newLabels, newValues];
-	}
+	let selectedParams: string[];
+	let checkedMean: boolean;
+	let parameterType: string;
 
 	// setup chart with empty config after canvas is mounted
 	onMount(() => {
@@ -174,7 +61,7 @@
 		let labels, values;
 
 		[labels, values] = calculateAxis(
-			$data.toCollection(),
+			$data.toCollection(true),
 			selectedParams,
 			checkedMean,
 			parameterType
--- graphgadget/src/lib/graphs/histogram/ParameterSelector.svelte
+++ graphgadget/src/lib/graphs/histogram/ParameterSelector.svelte
@@ -2,9 +2,14 @@
 	export let columnNames;
 	export let numericColumnNames;
 
-	export let selectedParams: string[];
-	export let parameterType: string;
-	export let checkedMean: boolean;
+	export let selectedParams: string[] = [];
+	export let parameterType: string = ABSOLUTE_FREQUENCY;
+	export let checkedMean: boolean = false;
+
+	import {
+		ABSOLUTE_FREQUENCY,
+		RELATIVE_FREQUENCY
+	} from '$lib/graphs/histogram/HistogramController';
 </script>
 
 <p>Parameters on the x-axis</p>
@@ -24,14 +29,14 @@
 <p>Parameter of the y-axis</p>
 
 <select data-testid="select-y-axis-parameter" bind:value={parameterType}>
-	<option value="Absolute Frequency">Absolute Frequency</option>
-	<option value="Relative Frequency">Relative Frequency</option>
+	<option value={ABSOLUTE_FREQUENCY}>{ABSOLUTE_FREQUENCY}</option>
+	<option value={RELATIVE_FREQUENCY}>{RELATIVE_FREQUENCY}</option>
 	{#each numericColumnNames as column}
 		<option value={column}>{column}</option>
 	{/each}
 </select>
 
-{#if parameterType != 'Absolute Frequency' && parameterType != 'Relative Frequency'}
+{#if parameterType != ABSOLUTE_FREQUENCY && parameterType != RELATIVE_FREQUENCY}
 	<label>
 		<input type="checkbox" data-testid="check-mean" name="mean" bind:checked={checkedMean} />
 		Mean
