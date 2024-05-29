955002562b3ad846d4b61eb39d70d6954ed78739
Emils Dzintars <E.Dzintars@student.tudelft.nl>
2024-05-29 16:16:20 +0300

    comment change

--- graphgadget/src/lib/Store.ts
+++ graphgadget/src/lib/Store.ts
@@ -10,11 +10,17 @@ export type BinDictionary = Record<string, number>;
 export const ABSOLUTE_FREQUENCY: string = 'Absolute Frequency';
 export const RELATIVE_FREQUENCY: string = 'Relative Frequency';
 
-//groupBy selected columns
+/**
+ * groupBy selected columns
+ */
 export const selectedColumns = writable<string[]>([]);
-//select values/columns
+/**
+ * select values/columns
+ */
 export const selectedValues = writable<string[]>([]);
-//bin size for each of the columns
+/**
+ * bin size for each of the columns
+ */
 export const binSizes = writable<BinDictionary>();
 export const checkedMean = writable<boolean>(false);
 //export const numericColumns = writable<[string, number][]>([]);
