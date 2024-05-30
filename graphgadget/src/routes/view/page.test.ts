03618d8d35efc0b97b68e78d39f39aae536bda75
Jang Hun Oh <edwardoh@tudelft.nl>
2024-05-30 17:28:21 +0200

    Changed tests for view

--- graphgadget/src/routes/view/page.test.ts
+++ graphgadget/src/routes/view/page.test.ts
@@ -1,8 +1,7 @@
-import { render } from '@testing-library/svelte';
+import { render, waitFor } from '@testing-library/svelte';
 import sut from './+page.svelte';
 import { describe, it, expect } from 'vitest';
 import { selected_graph } from '$lib/graphs/Store';
-import { get } from 'svelte/store';
 import Histogram from '$lib/graphs/histogram/Histogram.svelte';
 
 describe('View', () => {
@@ -10,36 +9,26 @@ describe('View', () => {
 		const { container } = render(sut);
 		expect(container).to.exist;
 	});
-	it('should contain histogram', async () => {
+
+	it('should contain placeholder text when no graph is selected', () => {
 		render(sut);
-		expect(get(selected_graph)).toBe(Histogram);
+		const placeholderText = document.querySelector('p.text-lg.text-gray-600');
+		expect(placeholderText).to.exist;
+		expect(placeholderText.textContent).toBe('Please select a graph to display.');
 	});
+
+	it('should render the selected graph component', async () => {
+		selected_graph.set(Histogram);
+		const { container } = render(sut);
+		await waitFor(() => {
+			const histogramComponent = container.querySelector('div.mt-5.w-full.flex.justify-center');
+			expect(histogramComponent).to.exist;
+		});
+	});
+
 	it('session storage cleared should still render', () => {
 		sessionStorage.clear();
 		const { container } = render(sut);
 		expect(container).to.exist;
 	});
-	// it('should have a select component', async () => {
-	// 	const { getByRole } = render(sut);
-	// 	const select = getByRole('combobox');
-	// 	expect(select).to.exist;
-	// });
-	// it('should display all graphs', async () => {
-	// 	GraphMetas.push({
-	// 		title: 'Test',
-	// 		description: 'Test',
-	// 		component: Histogram
-	// 	});
-	// 	const { getAllByRole } = render(sut);
-
-	// 	const options = getAllByRole('option') as HTMLOptionElement[];
-	// 	expect(options.length).to.equal(GraphMetas.length);
-	// 	expect(options.map((o) => o.textContent)).toEqual(GraphMetas.map((g) => g.title));
-	// });
-	// it('should be able to render no graphs', async () => {
-	// 	GraphMetas.length = 0;
-	// 	const { queryAllByRole } = render(sut);
-	// 	const options = queryAllByRole('option') as HTMLOptionElement[];
-	// 	expect(options.length).to.equal(0);
-	// });
 });
