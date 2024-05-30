18da19332e61c525ab5315c41d70cb7f3adbdaa9
Jang Hun Oh <edwardoh@tudelft.nl>
2024-05-30 15:56:55 +0200

    Added Export.svelte file

---
+++ graphgadget/src/lib/graphs/Export.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	import PngButton from '$lib/shared-components/PNGButton.svelte';
+	import JpgButton from '$lib/shared-components/JPGButton.svelte';
+	export let chart;
+</script>
+
+<div class="flex space-x-4">
+	<PngButton {chart} />
+	<JpgButton {chart} />
+</div>
