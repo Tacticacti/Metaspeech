62c1f2aff64447cf46aa62225d2f0f8d8ae339cb
Mateo <M.A.Nasse@student.tudelft.nl>
2024-05-30 12:59:43 +0200

    Created style for previous page and button to go back to home

--- graphgadget/src/routes/previous/+page.svelte
+++ graphgadget/src/routes/previous/+page.svelte
@@ -4,6 +4,7 @@
 	import { onMount } from 'svelte';
 	// @ts-expect-error Ill curse daily ts until this is fixed
 	import { goto } from '$app/navigation';
+	import { Button } from 'flowbite-svelte';
 
 	var storedDatasets: string[] = [];
 
@@ -71,19 +72,34 @@
 	}
 </script>
 
-<h1>Previous Data</h1>
+<main class="bg-offwhite min-h-screen max-h-screen flex justify-center">
 
-{#if storedDatasets.length > 0}
-	<ul>
-		{#each storedDatasets as dataset}
-			<li>
-				<button on:click={() => loadDataset(dataset)}>{dataset}</button>
-				<button on:click={() => deleteDataset(dataset)}>X</button>
-			</li>
-		{/each}
-	</ul>
-{:else}
-	<p>No previous data available.</p>
-{/if}
+	<div class="absolute top-0 left-0">
+		<Button href="/" class="bg-darkblue rounded-xl ml-4 mt-4 hover:bg-blue-800">
+			<img src="next.png" alt="go back arrow" class="rotate-180 w-8 invert">
+		</Button>
+	</div>
 
-<button on:click={clearCache}>Clear Data</button>
+	<div class="flex flex-col align-top items-center w-[80%] pt-8">
+		<div class="min-w-full flex flex-col items-center">
+			<h1 class="text-darkblue text-2xl font-semibold mb-8">Previous Data</h1>
+			<div class="bg-darkblue min-w-[80%] h-[4px] mb-8 rounded-full"></div>
+		</div>
+
+		{#if storedDatasets.length > 0}
+			<ul class="max-w-xl max-h-[60vh] overflow-y-auto font-medium w-full text-lg leading-none bg-darkblue border-blue-200 divide-y divide-blue-200 rounded-md">
+				{#each storedDatasets as dataset}
+					<li class="py-3.5 min-w-full max-w-full flex items-center justify-between mx-0 text-offwhite hover:text-blue-600 hover:bg-blue-300">
+						<span class="ml-6 mr-2.5 w-1 h-7 bg-blue-500 rounded-r-md"></span>
+						<button on:click={() => loadDataset(dataset)}>{dataset}</button>
+						<button class="px-4 mr-2 text-offwhite hover:bg-red-400 h-10 font-bold font-sans text-2xl rounded-full" on:click={() => deleteDataset(dataset)}>X</button>
+					</li>
+				{/each}
+			</ul>
+		{:else}
+			<p class=" text-xl font-semibold text-red-400">No previous data available.</p>
+		{/if}
+
+		<Button class="my-12 text-offwhite bg-red-400 px-12 py-2 text-base font-bold rounded-lg hover:bg-red-500" on:click={clearCache}>Clear Data</Button>
+	</div>
+</main>
