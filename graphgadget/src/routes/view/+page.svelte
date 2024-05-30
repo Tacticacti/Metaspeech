56aace76af7fbe2312164e4f8d2e2c711ca10e9b
Jang Hun Oh <edwardoh@tudelft.nl>
2024-05-30 13:46:02 +0200

    Added initial style for view page

--- graphgadget/src/routes/view/+page.svelte
+++ graphgadget/src/routes/view/+page.svelte
@@ -4,6 +4,7 @@
 	import { GraphMetas } from '$lib/graphs/Graphs';
 	import { beforeUpdate } from 'svelte';
 	import { loadSession } from '$lib/utils/SessionLoad';
+	import NavBar from '$lib/shared-components/NavBar.svelte';
 
 	/**
 	 * Will check if there is a dataframe in session storage and load it
@@ -13,8 +14,54 @@
 	});
 </script>
 
-<Grapher graphs={GraphMetas}></Grapher>
+<style>
 
-{#if $selected_graph}
-	<svelte:component this={$selected_graph} />
-{/if}
+	.graph-options {
+		display: flex;
+		justify-content: center;
+		margin: 20px 0;
+	}
+
+	.graph-description {
+		background-color: #d1e7dd;
+		padding: 20px;
+		border-radius: 8px;
+		margin-bottom: 20px;
+	}
+
+	.graph {
+		display: flex;
+		justify-content: center;
+		padding: 20px;
+		border: 1px solid #d1d5db;
+		border-radius: 8px;
+		background-color: white;
+		box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+	}
+</style>
+
+<main class="bg-offwhite">
+	<NavBar currentPage={'view'} />
+	<div class="flex flex-col justify-center pt-16 m-5 max-w-full items-center">
+		<div class="graph-options">
+			<Grapher graphs={GraphMetas}></Grapher>
+		</div>
+
+		{#if $selected_graph}
+			<div class="graph-description">
+				{console.log($selected_graph.name)}
+				{#if $selected_graph.name === 'Proxy<PieChart>'}
+					<p>Almost heaven, West Virginia</p>
+					<p>Blue Ridge Mountains, Shenandoah River</p>
+					<p>Life is old there, older than the trees</p>
+					<p>Younger than the mountains, growin' like a breeze</p>
+				{/if}
+			</div>
+
+			<div class="graph">
+				<svelte:component this={$selected_graph} />
+			</div>
+		{/if}
+	</div>
+
+</main>
