158c283437ba4a21b28b8d86be2016cabe3b74f7
Emils Dzintars <edzintars@student.tudelft.nl>
2024-05-31 16:54:14 +0200

    initial select style

--- graphgadget/src/lib/ColumnSelector/ColumnSelector.svelte
+++ graphgadget/src/lib/ColumnSelector/ColumnSelector.svelte
@@ -41,116 +41,126 @@
 	function toggleMean() {
 		$checkedMean = !$checkedMean;
 	}
-	/**
-	 *	removes the column from selectedValues if it is selected in groupby
-	 * @param name: name of the column
-	 * no return value
-	 */
-	function handleClickGroupBy(name: string) {
-		currentlySelectedValues.filter((n) => n !== name);
-	}
-	/**
-	 * removes the column from selectedColumns if it is selected in select
-	 * @param name: name of the column
-	 * no return value
-	 */
-	function handleClickSelect(name: string) {
-		currentlySelectedValues.filter((n) => n !== name);
-	}
 </script>
+<div class="flex justify-center">
+	<div>
+		<h3 class="mb-4 font-semibold text-gray-900 dark:text-white">Group by</h3>
+		<ul
+			class="w-48 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg dark:bg-gray-700 dark:border-gray-600 dark:text-white"
+		>
+			{#each availableForGroupby as column}
+				<li class="w-full border-b border-gray-200 rounded-t-lg dark:border-gray-600">
+					<div class="flex items-center ps-3">
+						<input
+							bind:group={currentlySelectedColumns}
+							data-testid="groupby-{column}"
+							value={column}
+							id="groupby-checkbox-{column}"
+							type="checkbox"
+							class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500"
+						/>
+						<label
+							for="groupby-checkbox-{column}"
+							class="w-full py-3 ms-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+							>{column}</label
+						>
+					</div>
+				</li>
+			{/each}
+		</ul>
+	</div>
+	<div>
+		<h3 class="mb-4 font-semibold text-gray-900 dark:text-white">Select</h3>
+		<ul
+			class="w-48 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg dark:bg-gray-700 dark:border-gray-600 dark:text-white"
+		>
+			{#each availableForSelect as column}
+				<li class="w-full border-b border-gray-200 rounded-t-lg dark:border-gray-600">
+					<div class="flex items-center ps-3">
+						<input
+							bind:group={currentlySelectedValues}
+							data-testid="select-{column}"
+							value={column}
+							id="select-checkbox-{column}"
+							type="checkbox"
+							class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500"
+						/>
+						<label
+							for="select-checkbox-{column}"
+							class="w-full py-3 ms-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+							>{column}</label
+						>
+					</div>
+				</li>
+			{/each}
+			<li class="w-full border-b border-gray-200 rounded-t-lg dark:border-gray-600">
+				<div class="flex items-center ps-3">
+					<input
+						bind:group={currentlySelectedValues}
+						data-testid="select-{ABSOLUTE_FREQUENCY}"
+						value={ABSOLUTE_FREQUENCY}
+						id="select-checkbox-{ABSOLUTE_FREQUENCY}"
+						type="checkbox"
+						class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500"
+					/>
+					<label
+						for="select-checkbox-{ABSOLUTE_FREQUENCY}"
+						class="w-full py-3 ms-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+						>{ABSOLUTE_FREQUENCY}</label
+					>
+				</div>
+			</li>
+			<li class="w-full border-b border-gray-200 rounded-t-lg dark:border-gray-600">
+				<div class="flex items-center ps-3">
+					<input
+						bind:group={currentlySelectedValues}
+						data-testid="select-{RELATIVE_FREQUENCY}"
+						value={RELATIVE_FREQUENCY}
+						id="select-checkbox-{RELATIVE_FREQUENCY}"
+						type="checkbox"
+						class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500"
+					/>
+					<label
+						for="select-checkbox-{RELATIVE_FREQUENCY}"
+						class="w-full py-3 ms-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+						>{RELATIVE_FREQUENCY}</label
+					>
+				</div>
+			</li>
+		</ul>
+	</div>
+	<div>
+		<h3 class="mb-4 font-semibold text-gray-900 dark:text-white">Binning</h3>
+		{#each numericColumns as column}
+			{#if currentlySelectedColumns.includes(column[0])}
+				<label class="block mb-4">
+					<span class="block text-gray-700 font-semibold mb-2">
+					{column[0]} Bin Size:
+					</span>
+					<input
+						type="number"
+						bind:value={currentBinSizes[column[0]]}
+						data-testid="number-bin-{column[0]}"
+						name={column[0]}
+						min="1"
+						max={Math.abs(column[1]) + 1}
+						class="block w-full p-2 border border-gray-300 rounded mb-2 focus:outline-none focus:ring focus:border-blue-300"
+					/>
+					<input
+						type="range"
+						bind:value={currentBinSizes[column[0]]}
+						data-testid="range-bin-{column[0]}"
+						name={column[0]}
+						min="1"
+						max={Math.abs(column[1]) + 1}
+						class="w-full"
+					/>
+				</label>
+			{/if}
+		{/each}
+	</div>
+</div>
 
-
-
-<h3 class="mb-4 font-semibold text-gray-900 dark:text-white">Group by</h3>
-<ul class="w-48 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg dark:bg-gray-700 dark:border-gray-600 dark:text-white">
-	{#each availableForGroupby as column}
-		<li class="w-full border-b border-gray-200 rounded-t-lg dark:border-gray-600">
-			<div class="flex items-center ps-3">
-				<input bind:group={currentlySelectedColumns} data-testid="groupby-{column}" value={column} id="vue-checkbox"  type="checkbox" value="" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500">
-				<label for="vue-checkbox" class="w-full py-3 ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">{column}</label>
-			</div>
-		</li>
-	{/each}
-    
-</ul>
-
-
-<!-- <p>Group by</p>
-{#each availableForGroupby as column}
-	<label>
-		<input
-			on:click={() => {
-				handleClickGroupBy(column);
-			}}
-			type="checkbox"
-			data-testid="groupby-{column}"
-			name="params"
-			value={column}
-			bind:group={currentlySelectedColumns}
-		/>
-		{column}
-	</label>
-{/each} -->
-{#each numericColumns as column}
-	{#if currentlySelectedColumns.includes(column[0])}
-		<br />
-		<label>
-			{column[0]} Bin Size:
-			<input
-				type="number"
-				bind:value={currentBinSizes[column[0]]}
-				data-testid="number-bin-{column[0]}"
-				name={column[0]}
-				min="1"
-				max={Math.abs(column[1]) + 1}
-			/>
-			<input
-				type="range"
-				bind:value={currentBinSizes[column[0]]}
-				data-testid="range-bin-{column[0]}"
-				name={column[0]}
-				min="1"
-				max={Math.abs(column[1]) + 1}
-			/>
-		</label>
-	{/if}
-{/each}
-<p>Select</p>
-{#each availableForSelect as column}
-	<label>
-		<input
-			on:click={() => {
-				handleClickSelect(column);
-			}}
-			type="checkbox"
-			data-testid="select-{column}"
-			name="params"
-			value={column}
-			bind:group={currentlySelectedValues}
-		/>
-		{column}
-	</label>
-{/each}
-<label>
-	<input
-		type="checkbox"
-		data-testid="select-{ABSOLUTE_FREQUENCY}"
-		name="params"
-		value={ABSOLUTE_FREQUENCY}
-		bind:group={currentlySelectedValues}
-	/>
-	{ABSOLUTE_FREQUENCY}
-</label>
-<label>
-	<input
-		type="checkbox"
-		data-testid="select-{RELATIVE_FREQUENCY}"
-		name="params"
-		value={RELATIVE_FREQUENCY}
-		bind:group={currentlySelectedValues}
-	/>
-	{RELATIVE_FREQUENCY}
-</label>
 
 <div>
 	<button data-testid="checked-mean-button" on:click={toggleMean}>
--- graphgadget/src/routes/select/+page.svelte
+++ graphgadget/src/routes/select/+page.svelte
@@ -10,16 +10,22 @@
 	beforeUpdate(() => {
 		loadSession();
 	});
-	// function handleClick() {
-	// 	// set the storage to the updated data
-	// 	sessionStorage.setItem('current-df', JSON.stringify($data));
-	// }
 </script>
+
 <main class="bg-offwhite max-w-full min-h-screen m-0">
 	<NavBar currentPage={'select'} />
-	<div class="pt-16 m-5 max-w-full">
-		<a href="/view" data-testid="next-link">Next</a>
+	<div class="flex flex-col pt-16 m-5 max-w-full">
+		<div
+			class="flex items-center justify-center self-end max-w-32 max-h-14 py-4 px-12 bg-darkblue rounded-lg hover:bg-blue-900"
+		>
+			<!-- Given that it was implemented with an a link, right now you need to click on the words to go to next page -->
+			<a
+				href="/view"
+				class=" text-offwhite font-bold rounded-lg text-sm mr-4"
+				data-testid="next-link">Next</a
+			>
+			<img src="next.png" class=" invert w-8 h-8" alt="Next icon" />
+		</div>
 		<ColumnSelector></ColumnSelector>
 	</div>
 </main>
-
