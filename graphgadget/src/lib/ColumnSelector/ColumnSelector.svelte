237ade6e1d3ae640b49aa69594a95be9f9c1a00d
Emils Dzintars <edzintars@student.tudelft.nl>
2024-05-31 14:53:19 +0200

    group by style

--- graphgadget/src/lib/ColumnSelector/ColumnSelector.svelte
+++ graphgadget/src/lib/ColumnSelector/ColumnSelector.svelte
@@ -59,7 +59,23 @@
 	}
 </script>
 
-<p>Group by</p>
+
+
+<h3 class="mb-4 font-semibold text-gray-900 dark:text-white">Group by</h3>
+<ul class="w-48 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg dark:bg-gray-700 dark:border-gray-600 dark:text-white">
+	{#each availableForGroupby as column}
+		<li class="w-full border-b border-gray-200 rounded-t-lg dark:border-gray-600">
+			<div class="flex items-center ps-3">
+				<input bind:group={currentlySelectedColumns} data-testid="groupby-{column}" value={column} id="vue-checkbox"  type="checkbox" value="" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500">
+				<label for="vue-checkbox" class="w-full py-3 ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">{column}</label>
+			</div>
+		</li>
+	{/each}
+    
+</ul>
+
+
+<!-- <p>Group by</p>
 {#each availableForGroupby as column}
 	<label>
 		<input
@@ -74,7 +90,7 @@
 		/>
 		{column}
 	</label>
-{/each}
+{/each} -->
 {#each numericColumns as column}
 	{#if currentlySelectedColumns.includes(column[0])}
 		<br />
--- graphgadget/src/routes/select/+page.svelte
+++ graphgadget/src/routes/select/+page.svelte
@@ -2,6 +2,7 @@
 	import { beforeUpdate } from 'svelte';
 	import { loadSession } from '$lib/utils/SessionLoad';
 	import ColumnSelector from '$lib/ColumnSelector/ColumnSelector.svelte';
+	import NavBar from '$lib/shared-components/NavBar.svelte';
 
 	/**
 	 * Will check if there is a dataframe in session storage and load it
@@ -14,7 +15,11 @@
 	// 	sessionStorage.setItem('current-df', JSON.stringify($data));
 	// }
 </script>
+<main class="bg-offwhite max-w-full min-h-screen m-0">
+	<NavBar currentPage={'select'} />
+	<div class="pt-16 m-5 max-w-full">
+		<a href="/view" data-testid="next-link">Next</a>
+		<ColumnSelector></ColumnSelector>
+	</div>
+</main>
 
-<a href="/view" data-testid="next-link">Next</a>
-
-<ColumnSelector></ColumnSelector>
