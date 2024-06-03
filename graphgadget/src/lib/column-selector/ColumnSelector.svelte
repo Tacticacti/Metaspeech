02c09cafe44171e80a675a977e298f68ca4846ce
Emils Dzintars <edzintars@student.tudelft.nl>
2024-06-03 23:18:31 +0200

    minor style change

--- graphgadget/src/lib/column-selector/ColumnSelector.svelte
+++ graphgadget/src/lib/column-selector/ColumnSelector.svelte
@@ -43,7 +43,7 @@
 </script>
 
 <div class="flex justify-center">
-	<div>
+	<div class="pr-5">
 		<h3 class="mb-4 font-semibold text-gray-900 dark:text-white">Group by</h3>
 		<ul
 			class="w-48 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg dark:bg-gray-700 dark:border-gray-600 dark:text-white"
@@ -65,7 +65,7 @@
 							>{column}</label
 						>
 						{#if currentlySelectedColumns.includes(column) && numericColumns.includes(column)}
-							<label class="w-full py-3 ms-2 text-sm font-medium">
+							<label class="w-full pr-1 py-3 ms-2 text-sm font-medium">
 								<input
 									type="number"
 									placeholder="Bin size"
