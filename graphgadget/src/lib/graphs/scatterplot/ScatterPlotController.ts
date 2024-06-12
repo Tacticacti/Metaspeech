903747c99171476f87805712e7c6c9e00a2f48a0
Pedro Gomes Moreira <p.gomesmoreira@student.tudelft.nl>
2024-06-12 20:01:42 +0200

    finished code, not working properly

--- graphgadget/src/lib/graphs/scatterplot/ScatterPlotController.ts
+++ graphgadget/src/lib/graphs/scatterplot/ScatterPlotController.ts
@@ -18,8 +18,52 @@ export function getLegendCol(groupedColumns: Column[], xCol: Column) : Column | 
     return undefined;
 }
 
-function getScatterMap(groupedData: GroupedDataFrame, yAxisCol: Column, xAxisCol: Column, legendCol: Column | undefined): Map<string | number, [number, number][]> {
-    throw new Error('TODO');
+function getIndicesOfColumns(groupedData: GroupedDataFrame, xAxisCol: Column, legendCol: Column | undefined): [number, number] {
+    let i = 0, indexX = -1, indexLegend = -1;
+
+    for (const col of groupedData.groupedColumns) {
+        if (col === xAxisCol) {
+            indexX = i;
+        } else if (col === legendCol) {
+            indexLegend = i;
+        }  
+        ++i;
+    }    
+
+    return [indexX, indexLegend];
+}
+
+function getScatterMap(groupedData: GroupedDataFrame, yAxisCol: Column, xAxisCol: Column, legendCol: Column | undefined): Map<string, [number, number][]> {
+    const [indexX, indexLegend] = getIndicesOfColumns(groupedData, xAxisCol, legendCol);
+    const map = new Map<string, [number, number][]>();
+
+    for (const group of groupedData.groups) {
+        let key = '';
+
+        if (legendCol === undefined) {
+            // Use x-axis column name as key
+            key = xAxisCol.name;
+        } else {
+            // Use legend value as key
+            key = group.keys[indexLegend]?.toString() ?? '';
+        }
+
+        for (const val of group.values) {
+            // We know that val is of a numeric type since it is the y-axis
+            const yVal = val as number;
+            // We also know that the x-axis column is of numeric type
+            const xVal = group.keys[indexX] as number;
+            const dataPoint: [number, number] = [xVal, yVal];
+
+            if (map.has(key)) {
+                map.get(key)!.push(dataPoint);
+            } else {
+                map.set(key, [dataPoint]);
+            }
+        }
+    }
+
+    return map;
 }
 
 export function getScatterDatasets(groupedData: GroupedDataFrame, yAxisCol: Column, xAxisCol: Column, legendCol: Column | undefined, styles: ScatterStyle[]): ScatterDataset[] {
@@ -31,7 +75,7 @@ export function getScatterDatasets(groupedData: GroupedDataFrame, yAxisCol: Colu
 
     for (let i = 0; i < legendValues.length; ++i) {
         const style = styles[i % styles.length];
-        const label = legendValues[i] + '';
+        const label = legendValues[i];
         const data = scatterMap.get(legendValues[i])!;
 
         scatterDataset.push({data, label, style});
