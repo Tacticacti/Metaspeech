dd1ef5db30f935a46f0d6e221cd993cb713ed7ad
Mateo <M.A.Nasse@student.tudelft.nl>
2024-06-13 15:39:46 +0200

    Select page style and bin sizes changes

--- metaspeech/src/routes/select/+page.svelte
+++ metaspeech/src/routes/select/+page.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
 	import { df } from '$lib/Store';
-	import type { Column, DataTypeString } from '$lib/Types';
+	import type { Column } from '$lib/Types';
+	import { Button } from 'flowbite-svelte';
+	import nextImg from '$assets/icons/next.png';
 
 	const columns = df.columns;
 	$: aggregatableColumns = $columns.filter((c) => c.type === 'number' && !c.groupBy);
@@ -8,18 +10,15 @@
 
 	$: selectColumnForAggregation(aggregateBy);
 
-	function getGroupingOptions(type: DataTypeString): string[] {
-		const typeOptions = {
-			string: ['specific'],
-			number: ['specific', 'binned']
-		};
-		return typeOptions[type] ?? [];
-	}
-
 	function selectColumnForGrouping(column: Column, select: boolean) {
-		column.groupBy = select ? { type: 'specific' } : undefined;
+		column.groupBy = select
+			? column.type === 'number'
+				? { type: 'binned', size: 1 }
+				: { type: 'specific' }
+			: undefined;
 		df.forceStoreUpdate();
 	}
+
 	function translateIndex(index: number) {
 		let count = -1;
 		for (let i = 0; i < $columns.length; i++) {
@@ -40,43 +39,79 @@
 	}
 </script>
 
-<a href="/view" data-testid="next-link">Next</a>
-<div class="flex">
-	<div class="flex flex-col">
-		{#each $columns as column}
-			<span>
-				<label>
-					{column.name}
-					<input
-						type="checkbox"
-						on:input={(e) => selectColumnForGrouping(column, e.currentTarget.checked)}
-						checked={Boolean(column.groupBy)}
-					/>
-				</label>
-				{#if column.groupBy}
-					<select bind:value={column.groupBy.type}>
-						{#each getGroupingOptions(column.type) as groupingOption}
-							<option value={groupingOption}>{groupingOption}</option>
-						{/each}
-					</select>
-					{#if column.groupBy.type === 'binned'}
-						<input type="number" bind:value={column.groupBy.size} />
-					{/if}
-				{/if}
-			</span>
-		{/each}
-	</div>
+<div class="flex h-[90vh] flex-col items-center justify-around">
+	<div class="flex w-full justify-center border-4 py-5">
+		<div class="mx-20 flex w-full max-w-[100vh] flex-col items-center">
+			<span class="text-xl font-bold"> GROUP BY </span>
+			<div class="mt-5 flex w-fit flex-col">
+				{#each $columns as column}
+					<span class=" mb-2 flex w-full min-w-96 items-center justify-between">
+						<label class="flex items-center">
+							<input
+								class="mr-2 h-5 w-5 rounded-xl border-2 transition-all duration-200 ease-in-out focus:invisible"
+								type="checkbox"
+								on:input={(e) => selectColumnForGrouping(column, e.currentTarget.checked)}
+								checked={Boolean(column.groupBy)}
+							/>
+							<span class="w-36 truncate">
+								{column.name}
+							</span>
+						</label>
+						{#if column.groupBy && column.groupBy.type === 'binned'}
+							<div class="ml-10">
+								<span>Bin Size:</span>
+								<input
+									class="remove-arrow max-h-6 max-w-16 rounded-lg border-0 bg-darkblue text-center font-medium text-offwhite"
+									type="number"
+									placeholder="1"
+									bind:value={column.groupBy.size}
+								/>
+							</div>
+						{/if}
+					</span>
+				{/each}
+			</div>
+		</div>
 
-	<div class="flex flex-col">
-		<label>
-			Frequency
-			<input type="radio" bind:group={aggregateBy} value={-1} />
-		</label>
-		{#each aggregatableColumns as column, index}
-			<label>
-				{column.name}
-				<input type="radio" bind:group={aggregateBy} value={index} />
-			</label>
-		{/each}
+		<div class="size-3 h-full rounded-lg bg-gray-200"></div>
+
+		<div class="mx-20 flex w-full max-w-[100vh] flex-col items-center">
+			<span class="text-xl font-bold"> SELECT </span>
+			<div class="mt-5 flex w-fit flex-col">
+				<label class="mb-2">
+					<input
+						class="mr-1 h-5 w-5 border-2 transition-all duration-200 ease-in-out focus:invisible"
+						type="radio"
+						bind:group={aggregateBy}
+						value={-1}
+					/>
+					Frequency
+				</label>
+				<div class="mb-2 size-1 w-full rounded-lg bg-gray-300"></div>
+				{#each aggregatableColumns as column, index}
+					<label class="mb-2 flex items-center">
+						<input
+							class="mr-2 h-5 w-5 border-2 transition-all duration-200 ease-in-out focus:invisible"
+							type="radio"
+							bind:group={aggregateBy}
+							value={index}
+						/>
+						<span class="w-64 truncate">
+							{column.name}
+						</span>
+					</label>
+				{/each}
+			</div>
+		</div>
 	</div>
+	<Button
+		href="/view"
+		class="w-48 rounded-lg bg-darkblue text-sm font-bold text-offwhite"
+		data-testid="next-link"
+	>
+		<div class="flex items-center justify-center">
+			Next
+			<img src={nextImg} class=" ml-4 h-8 w-8 invert" alt="Next icon" />
+		</div>
+	</Button>
 </div>
