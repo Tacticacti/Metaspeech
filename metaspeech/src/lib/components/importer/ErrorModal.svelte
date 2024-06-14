1aedf975fc67ce118a3d4ec2912b0fcee03522fe
Jang Hun Oh <edwardoh@tudelft.nl>
2024-06-14 16:00:58 +0200

    Fixed checkstyle

--- metaspeech/src/lib/components/importer/ErrorModal.svelte
+++ metaspeech/src/lib/components/importer/ErrorModal.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
 	import { createEventDispatcher } from 'svelte';
 	import { CloseButton } from 'flowbite-svelte';
-  
+
 	/**
 	 * The error message to display in the modal
 	 */
@@ -10,23 +10,27 @@
 	 * Whether the modal is visible
 	 */
 	export let visible = true;
-  
+
 	const dispatch = createEventDispatcher();
-  
+
 	/**
 	 * Close the modal
 	 */
 	function closeModal() {
-	  dispatch('close');
+		dispatch('close');
 	}
-  </script>
-  
-  {#if visible}
-	<div class="fixed inset-0 flex items-center justify-center bg-black bg-opacity-40 z-50" data-testid="error-modal">
-	  <div class="bg-white p-6 rounded shadow-lg w-4/5 md:w-1/2 lg:w-1/3 flex items-center justify-between">
-		<p>{message}</p>
-		<CloseButton on:click={closeModal} class="close" data-testid="close-button" />
-	  </div>
+</script>
+
+{#if visible}
+	<div
+		class="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-40"
+		data-testid="error-modal"
+	>
+		<div
+			class="flex w-4/5 items-center justify-between rounded bg-white p-6 shadow-lg md:w-1/2 lg:w-1/3"
+		>
+			<p>{message}</p>
+			<CloseButton on:click={closeModal} class="close" data-testid="close-button" />
+		</div>
 	</div>
-  {/if}
-  
+{/if}
--- metaspeech/src/lib/components/importer/Importer.svelte
+++ metaspeech/src/lib/components/importer/Importer.svelte
@@ -6,14 +6,14 @@
 	import { UnsupportedFileError } from '$lib/Types';
 	import { GetFileExtension } from '$components/importer/scripts/FileParser';
 	import ErrorModal from './ErrorModal.svelte';
-  
+
 	const dispatch = createEventDispatcher<{
-	  input: DataFile;
+		input: DataFile;
 	}>();
-  
-	let errorMessage: string | null = null;
-	let isModalVisible = false;
-  
+
+	export let errorMessage: string | null = null;
+	export let isModalVisible = false;
+
 	/**
 	 * Handle the input event by parsing the file and dispatching the input event.
 	 * If the file is unsupported or an error occurs during parsing, an error message is shown in a modal.
@@ -21,52 +21,51 @@
 	 * @param event - The input event triggered when a file is selected.
 	 */
 	async function onInput(event: Event) {
-	  errorMessage = null; // Reset error message
-  
-	  // Get the file from the input event
-	  const input = event.target as HTMLInputElement;
-	  const file = input.files?.[0];
-	  if (!file) return;
-  
-	  try {
-		// Parse the file
-		const data: DataFrameLike = JSON.parse(JSON.stringify(await Parse(file)));
-  
-		const bundle = {
-		  data,
-		  name: file.name
-		};
-		// Dispatch the parsed data
-		dispatch('input', bundle);
-	  } catch (error) {
-		// Handle unsupported file error and other errors
-		if (!(error instanceof UnsupportedFileError)) {
-		  errorMessage = 'An unknown error occurred while processing the file.';
-		} else {
-		  errorMessage = `Invalid file input! .${GetFileExtension(file)} is not supported in this application!`;
+		errorMessage = null; // Reset error message
+
+		// Get the file from the input event
+		const input = event.target as HTMLInputElement;
+		const file = input.files?.[0];
+		if (!file) return;
+
+		try {
+			// Parse the file
+			const data: DataFrameLike = JSON.parse(JSON.stringify(await Parse(file)));
+
+			const bundle = {
+				data,
+				name: file.name
+			};
+			// Dispatch the parsed data
+			dispatch('input', bundle);
+		} catch (error) {
+			// Handle unsupported file error and other errors
+			if (error instanceof UnsupportedFileError) {
+				errorMessage = `Invalid file input! .${GetFileExtension(file)} is not supported in this application!`;
+			} else {
+				errorMessage = 'An unknown error occurred while processing the file.';
+			}
+			// Show the error modal
+			isModalVisible = true;
 		}
-		// Show the error modal
-		isModalVisible = true;
-	  }
 	}
-  
+
 	/**
 	 * Close the error modal and reset the error message.
 	 */
 	function closeModal() {
-	  isModalVisible = false;
-	  errorMessage = null;
+		isModalVisible = false;
+		errorMessage = null;
 	}
-  </script>
-  
-  <input
+</script>
+
+<input
 	data-testid="input"
 	on:input={onInput}
 	type="file"
 	accept=".tsv, .xls, .xlsx, .json, .csv, .txt"
 	hidden
 	{...$$restProps}
-  />
-  
-  <ErrorModal message={errorMessage} visible={isModalVisible} on:close={closeModal} />
-  
+/>
+
+<ErrorModal message={errorMessage} visible={isModalVisible} on:close={closeModal} />
